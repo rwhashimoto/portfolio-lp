aea0c4301913cda28e71413dd69471e42e344822
hashimoto <m.hashimoto@risewill.co.jp>
2021-09-15 17:54:54 +0900

    [mod]スムーススクロール実装

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-$("#contact").submit(function(){
-    if ($("input[name='entry.129489804']").val() == '') {
-      alert('正しいメールアドレスを入力してください');
-      return false;
-    } else {
-      $("#contact").submit();
-    }
+$(function(){//smooth scroll
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
   });
+});
