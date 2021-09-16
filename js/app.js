0023447584daa9f47c22b8e4aa7fb5426d54fad7
koya <koya.a.85@gmail.com>
2021-09-16 20:19:00 +0900

    [wip]途中まで

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
+$(function () {
+  // 送信ボタンを無効化
+  $('#submit').prop("disabled", true);
+
+  // 必須項目チェック
+  $('.q-valid').change(function () {
+      let flag = true;
+
+      // 値が間違っている場合はフラグをfalseにする
+      $('.q-valid').each(function (e) {
+          if ($('.q-required').val().match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/)){
+              flag = false;
+          }
+      });
+
+      // フラグがtrueの場合は送信ボタン有効
+      if (flag) {
+          $('#submit').prop("disabled", false);
+
+          $('#submit').on("click", function() {
+              window.location.href = "thanks.html";
+          });
+
+      } else {
+          $('#submit').prop("disabled", true);
+      }
+  });
+});
+
+
+$('#submit').on('click', function() {
+  window.location='thanks.html';
+});
+
 $(function(){//smooth scroll
   $('a[href^="#"]').click(function(){
     var speed = 500;
