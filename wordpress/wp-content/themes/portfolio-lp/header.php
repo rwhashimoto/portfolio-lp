519b89c8845d5349da7f52a62a3f1d22184b9ebf
hashimoto <m.hashimoto@risewill.co.jp>
2021-10-25 11:06:41 +0900

    [mod]headerパス変更

--- wordpress/wp-content/themes/portfolio-lp/header.php
+++ wordpress/wp-content/themes/portfolio-lp/header.php
@@ -21,8 +21,8 @@
 		<nav>
 			<ul  class="l-header-items">
 				<li class="l-header-item"><a href="<?php echo home_url('/about/'); ?>">About</a></li>
-				<li class="l-header-item"><a href="#works">Works</a></li>
-				<li class="l-header-item"><a href="#news">News</a></li>
+				<li class="l-header-item"><a href="<?php echo home_url('/works/'); ?>">Works</a></li>
+				<li class="l-header-item"><a href="<?php echo home_url('/news/'); ?>">News</a></li>
 				<li class="l-header-item"><a href="<?php echo home_url('/contact/'); ?>">Contact</a></li>
 				<li class="l-header-item ico-sns"><a href="DUMMY"><img src="<?= get_template_directory_uri(); ?>/assets/images/icon-instagram.png" alt="ico-instagram"></a></li>
 			</ul>
