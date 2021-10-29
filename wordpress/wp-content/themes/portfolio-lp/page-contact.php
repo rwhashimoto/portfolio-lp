edcd845c387654ffd077da4e4daffa9e199bbcf4
hashimoto <m.hashimoto@risewill.co.jp>
2021-10-29 16:08:00 +0900

    [wip]contactいったん

---
+++ wordpress/wp-content/themes/portfolio-lp/page-contact.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<section id="contact" class="l-main-content">
+	<h2 class="l-main-title">Contact</h2>
+    <?php the_content(); ?>
+</section>
+
+<?php get_footer(); ?>
--- wordpress/wp-content/themes/portfolio-lp/page.php
+++ wordpress/wp-content/themes/portfolio-lp/page.php
@@ -1,5 +1,5 @@
-<?php get_header(); ?>
+<!-- <?php get_header(); ?>
 <div class="l-main">
     <?php the_content(); ?>
 </div>
-<?php get_footer(); ?>
+<?php get_footer(); ?> -->
