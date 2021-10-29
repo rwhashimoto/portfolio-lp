7114d4cc866ce5d688e2ae3b82cb621e70ff2240
hashimoto <m.hashimoto@risewill.co.jp>
2021-10-29 19:16:08 +0900

    [mod]もろもろ

--- wordpress/wp-content/themes/portfolio-lp/single-news.php
+++ wordpress/wp-content/themes/portfolio-lp/single-news.php
@@ -3,7 +3,7 @@
 <section id="news" class="l-main-content single-news">
     <h2 class="l-main-title"><?= get_the_title(); ?></h2>
     <p><?= get_the_date(); ?></p>
-    <p class="entry-field"><?php the_content(); ?></p>
+    <div class="entry-field"><?php the_content(); ?></div>
     <a class="btn-prev" href="<?php echo home_url('/news/'); ?>">一覧に戻る</a>
 </section>
 
