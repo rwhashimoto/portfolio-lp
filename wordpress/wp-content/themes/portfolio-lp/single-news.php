eb18b083a5e814cc6a33949634f0e4ed00bb9a6f
hashimoto <m.hashimoto@risewill.co.jp>
2021-10-29 15:05:48 +0900

    [wip]news詳細いったん

--- wordpress/wp-content/themes/portfolio-lp/single-news.php
+++ wordpress/wp-content/themes/portfolio-lp/single-news.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
-<h2 class="l-main-title"><?= get_the_title(); ?></h2>
-<p><?= get_the_date(); ?></p>
-<p><?php the_content(); ?></p>
+
+<section id="news" class="l-main-content single-news">
+    <h2 class="l-main-title"><?= get_the_title(); ?></h2>
+    <p><?= get_the_date(); ?></p>
+    <p class="entry-field"><?php the_content(); ?></p>
+    <a class="btn-prev" href="<?php echo home_url('/news/'); ?>">一覧に戻る</a>
+</section>
+
 <?php get_footer(); ?>
